3a6d5a95ebca754d80a87b197ebd31dd567e0fda
Kelech Okereke Collins <postercolor444@gmail.com>
2026-02-24 00:59:24 +0100

    utils added and updated

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -22,8 +22,7 @@ def read_yaml (path_to_yaml: Path) -> ConfigBox:
     Raises:
         ValueError: if yaml file is empty
         e: empty file
-        
-        
+            
     Returns:
         ConfigBox: ConfigBox type
         
@@ -45,7 +44,6 @@ def read_yaml (path_to_yaml: Path) -> ConfigBox:
 def create_directories (path_to_directories: list, verbose=True):
     """create list of directories
     
-    
     Args:
     path_to_directories (list): list of path of directories
     ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False 
@@ -53,7 +51,7 @@ def create_directories (path_to_directories: list, verbose=True):
         
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
-        if verbse:
+        if verbose:
             logger.info(f"created directory at: {path}")
             
             
@@ -61,7 +59,6 @@ def create_directories (path_to_directories: list, verbose=True):
 @ensure_annotations
 def save_json(path: Path, data: dict):
     """save json data
-    
     
     Args:
         path (Path): path to json file
@@ -71,3 +68,76 @@ def save_json(path: Path, data: dict):
         json.dump(data, f, indent=4)
         
     logger.info(f"json file saved at: {path}")
+    
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """load json files data
+    
+    Args:
+        path (Path): path to json file
+        
+    Returns:
+        ConfigBox data as class attribute instead of dict
+    """
+    
+    with open(path) as f:
+        content = json.load(f)
+        
+    logger.info(f"json file loaded successfully from {path}")
+    return ConfigBox(content)
+
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    """save binary file
+    
+    Args:
+        data (Any): data to be saved as binary
+        path (Path): path to binary file
+    """
+    joblib.dump(value=data, filename=path)
+    logger.info(f"binary file saved at {path}")
+    
+    
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    """load binary data
+    
+    Args:
+        path (Path): path to binary file
+        
+    Returns:
+        Any: object stored in the file
+    """
+    data = joblib.load(path)
+    logger.info(f"binary file loaded from {path}")
+    return data
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in KB
+    
+    Args:
+        path (Path): path of the file
+        
+    Returns:
+        str: size in KB
+    """
+    
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~(size_in_kb) KB"
+
+
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open(fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+        
+        
+def encodeImageIntoBase64 (croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
+        
+        
