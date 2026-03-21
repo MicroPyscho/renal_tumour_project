ed30dfd36f32f7e39df4d234dbd6eb73b64107f1
Kelech Okereke Collins <postercolor444@gmail.com>
2026-03-21 23:52:01 +0100

    revise

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTraining
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
-        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
-    except Exception as e:
-        raise e
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    raise e
+    
